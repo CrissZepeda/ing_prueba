8680877d3c1c941cca6d2ef72b6929d46fa48b16
Cristhian Zepeda <criss.zep@gmail.com>
2022-12-02 00:52:06 -0300

    All added 3000 port

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ config();
 MONGO_ATLAS_URI = mongodb+srv://ingSoftware:admin88@ingsoftware.3xqr2uy.mongodb.net/petroleo?retryWrites=true&w=majority */
 
 export default {
-    port: process.env.PORT_PETROLEO || 80,
+    port: process.env.PORT_PETROLEO || 3000,
     //db_mongo: process.env.MONGO_LOCAL_URI,
     db_mongo:
         "mongodb+srv://ingSoftware:admin88@ingsoftware.3xqr2uy.mongodb.net/petroleo?retryWrites=true&w=majority",
